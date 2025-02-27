7309d5805ae73b94827975c21ba0756f9108fc07
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2025-02-27 23:45:03 +0100

    Add files via upload
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ modules/php.go
@@ -0,0 +1,92 @@
+package modules
+
+import (
+	"PreFlight/utils"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+type PhpModule struct{}
+
+func (p PhpModule) Name() string {
+	return "PHP"
+}
+
+func (p PhpModule) CheckRequirements(context map[string]interface{}) (errors []string, warnings []string, successes []string) {
+	// CHECK IF PHP IS INSTALLED.
+	phpInstalled, phpVersion := isPhpInstalled()
+
+	if phpInstalled {
+		successes = append(successes, fmt.Sprintf("PHP is installed with version: %s.", phpVersion))
+	} else {
+		errors = append(errors, "PHP is not installed. Please install PHP.")
+		return errors, warnings, successes
+	}
+
+	phpVersionRequirement, requiredExtensions, _, found := utils.ReadComposerJSON()
+
+	if !found {
+		warnings = append(warnings, "composer.json not found. PHP requirements cannot be dynamically determined.")
+		return errors, warnings, successes
+	}
+
+	if phpVersionRequirement != "" {
+		if !strings.Contains(phpVersion, phpVersionRequirement) {
+			errors = append(errors, fmt.Sprintf(
+				"PHP version %s is required, but version %s is installed.",
+				phpVersionRequirement, phpVersion,
+			))
+		} else {
+			successes = append(successes, fmt.Sprintf("Installed PHP version matches the required version: %s.", phpVersionRequirement))
+		}
+	}
+
+	for _, ext := range requiredExtensions {
+		if !CheckPHPExtension(ext) {
+			errors = append(errors, fmt.Sprintf("PHP extension %s is missing. Please enable it.", ext))
+		} else {
+			successes = append(successes, fmt.Sprintf("PHP extension %s is installed.", ext))
+		}
+	}
+
+	return errors, warnings, successes
+}
+
+// CHECK IF PHP IS INSTALLED AND RETURN THE VERSION IF AVAILABLE.
+func isPhpInstalled() (bool, string) {
+	cmd := exec.Command("php", "--version")
+	output, err := cmd.Output()
+
+	if err != nil {
+
+		return false, ""
+	}
+
+	lines := strings.Split(string(output), "\n")
+
+	if len(lines) > 0 {
+		versionLine := lines[0]
+		return true, versionLine
+	}
+
+	return true, ""
+}
+
+// CheckPHPExtension CHECK IF A SPECIFIC PHP EXTENSION IS INSTALLED.
+func CheckPHPExtension(extension string) bool {
+	cmd := exec.Command("php", "-m")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return false
+	}
+
+	for _, ext := range strings.Split(string(output), "\n") {
+		if strings.TrimSpace(ext) == extension {
+			return true
+		}
+	}
+
+	return false
+}
