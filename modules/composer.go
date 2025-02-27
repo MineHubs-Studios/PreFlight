5e1c2877b62719c8ad9e0a7e3a2fb9154620281c
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2025-02-27 23:44:47 +0100

    Update composer.go
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- modules/composer.go
+++ modules/composer.go
@@ -1,11 +1,10 @@
 package modules
 
 import (
-	"encoding/json"
+	"PreFlight/utils"
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 type ComposerModule struct{}
@@ -15,17 +14,8 @@ func (c ComposerModule) Name() string {
 }
 
 func (c ComposerModule) CheckRequirements(context map[string]interface{}) (errors []string, warnings []string, successes []string) {
-	// CHECK IF PHP IS INSTALLED.
-	phpInstalled, phpVersion := isPhpInstalled()
-
-	if phpInstalled {
-		successes = append(successes, fmt.Sprintf("PHP is installed with version: %s.", phpVersion))
-	} else {
-		errors = append(errors, "PHP is not installed. Please install PHP to use Composer.")
-	}
-
 	// READ composer.json AND PARSE THE REQUIRED INFORMATION.
-	_, phpExtensions, composerDeps, found := ReadComposerJSON()
+	_, _, composerDeps, found := utils.ReadComposerJSON()
 
 	// HANDLE MISSING composer.json.
 	if !found {
@@ -43,15 +33,6 @@ func (c ComposerModule) CheckRequirements(context map[string]interface{}) (error
 
 	successes = append(successes, "composer.json found.")
 
-	// CHECK PHP EXTENSIONS.
-	for _, ext := range phpExtensions {
-		if !CheckPHPExtension(ext) {
-			errors = append(errors, fmt.Sprintf("PHP extension %s is missing. Please enable it.", ext))
-		} else {
-			successes = append(successes, fmt.Sprintf("PHP extension %s is installed.", ext))
-		}
-	}
-
 	// CHECK COMPOSER DEPENDENCIES.
 	for _, dep := range composerDeps {
 		if !CheckComposerPackage(dep) {
@@ -62,90 +43,6 @@ func (c ComposerModule) CheckRequirements(context map[string]interface{}) (error
 	}
 
 	return errors, warnings, successes
-}
-
-// CHECK IF PHP IS INSTALLED AND RETURN THE VERSION IF AVAILABLE.
-func isPhpInstalled() (bool, string) {
-	cmd := exec.Command("php", "--version")
-	output, err := cmd.Output()
-
-	if err != nil {
-		return false, ""
-	}
-
-	lines := strings.Split(string(output), "\n")
-
-	if len(lines) > 0 {
-		versionLine := lines[0]
-		return true, versionLine
-	}
-
-	return true, ""
-}
-
-// CheckPHPExtension CHECK IF A SPECIFIC PHP EXTENSION IS INSTALLED.
-func CheckPHPExtension(extension string) bool {
-	cmd := exec.Command("php", "-m")
-	output, err := cmd.Output()
-
-	if err != nil {
-		return false
-	}
-
-	for _, ext := range strings.Split(string(output), "\n") {
-		if strings.TrimSpace(ext) == extension {
-			return true
-		}
-	}
-
-	return false
-}
-
-// ReadComposerJSON READ composer.json, PARSE REQUIRED PHP VERSION, EXTENSIONS, AND DEPENDENCIES.
-func ReadComposerJSON() (string, []string, []string, bool) {
-	var phpVersion string
-	var phpExtensions []string
-	var composerDeps []string
-
-	// CHECK IF composer.json EXISTS.
-	if _, err := os.Stat("composer.json"); os.IsNotExist(err) {
-		return "", phpExtensions, composerDeps, false
-	}
-
-	// READ THE composer.json FILE.
-	file, err := os.ReadFile("composer.json")
-
-	if err != nil {
-		return "", phpExtensions, composerDeps, false
-	}
-
-	// PARSE JSON CONTENT FROM composer.json.
-	var data map[string]interface{}
-
-	if err := json.Unmarshal(file, &data); err != nil {
-		return "", phpExtensions, composerDeps, false
-	}
-
-	// EXTRACT "require" AND "require-dev" SECTIONS.
-	if require, ok := data["require"].(map[string]interface{}); ok {
-		for dep, version := range require {
-			if dep == "php" {
-				phpVersion = fmt.Sprintf("%v", version)
-			} else if strings.HasPrefix(dep, "ext-") {
-				phpExtensions = append(phpExtensions, strings.TrimPrefix(dep, "ext-"))
-			} else {
-				composerDeps = append(composerDeps, dep)
-			}
-		}
-	}
-
-	if requireDev, ok := data["require-dev"].(map[string]interface{}); ok {
-		for dep := range requireDev {
-			composerDeps = append(composerDeps, dep)
-		}
-	}
-
-	return phpVersion, phpExtensions, composerDeps, true
 }
 
 // CheckComposerPackage CHECK IF A SPECIFIC COMPOSER PACKAGE IS INSTALLED.
