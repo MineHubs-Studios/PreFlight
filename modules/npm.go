67a460295ec472079f2e3b3b08962ed1b06ca10d
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2025-02-27 23:52:16 +0100

    Update npm.go
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- modules/npm.go
+++ modules/npm.go
@@ -1,8 +1,8 @@
 package modules
 
 import (
+	"PreFlight/utils"
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
@@ -22,7 +22,7 @@ func (n NpmModule) CheckRequirements(context map[string]interface{}) (errors []s
 	nodeVersionOutput := isNodeInstalled(&errors, &successes)
 
 	// READ package.json TO EXTRACT REQUIRED NODE VERSION AND DEPENDENCIES.
-	requiredNodeVersion, packageFound, requiredDeps := ReadPackageJSON()
+	requiredNodeVersion, packageFound, requiredDeps := utils.ReadPackageJSON()
 
 	// HANDLE MISSING package.json.
 	if !packageFound {
@@ -73,54 +73,6 @@ func isNodeInstalled(errors *[]string, successes *[]string) string {
 	*successes = append(*successes, fmt.Sprintf("Node.js is installed with version %s.", installedVersion))
 
 	return installedVersion
-}
-
-// ReadPackageJSON READ package.json TO EXTRACT NODE VERSION, DEPENDENCIES, AND DEV DEPENDENCIES.
-func ReadPackageJSON() (string, bool, []string) {
-	// CHECK IF package.json EXISTS.
-	if _, err := os.Stat("package.json"); os.IsNotExist(err) {
-		return "", false, nil
-	}
-
-	// READ package.json FILE CONTENT.
-	file, err := os.ReadFile("package.json")
-
-	if err != nil {
-		return "", false, nil
-	}
-
-	// PARSE JSON CONTENT FROM package.json.
-	var data map[string]interface{}
-
-	if err := json.Unmarshal(file, &data); err != nil {
-		return "", false, nil
-	}
-
-	// EXTRACT REQUIRED NODE VERSION FROM "engines" SECTION.
-	var requiredNodeVersion string
-
-	if engines, ok := data["engines"].(map[string]interface{}); ok {
-		if node, exists := engines["node"].(string); exists {
-			requiredNodeVersion = strings.TrimSpace(node)
-		}
-	}
-
-	// EXTRACT DEPENDENCIES AND DEV DEPENDENCIES.
-	var requiredDeps []string
-
-	if dependencies, ok := data["dependencies"].(map[string]interface{}); ok {
-		for dep := range dependencies {
-			requiredDeps = append(requiredDeps, dep)
-		}
-	}
-
-	if devDependencies, ok := data["devDependencies"].(map[string]interface{}); ok {
-		for dep := range devDependencies {
-			requiredDeps = append(requiredDeps, dep)
-		}
-	}
-
-	return requiredNodeVersion, true, requiredDeps
 }
 
 // HANDLE MISSING package.json BY CHECKING FOR LOCK FILES.
