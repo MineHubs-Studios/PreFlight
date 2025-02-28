32fabb47982ebf7447315e42e501115074c93792
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2025-02-28 12:10:57 +0100

    Update root.go
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,13 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "PreFlight",
 	Short: "PreFlight is a CLI tool for checking project dependencies",
-	Long: `PreFlight helps developers check, install, and manage 
+	Long: `PreFlight helps developers check, install, and manage
 project dependencies dynamically based on configuration files.`,
 }
 
