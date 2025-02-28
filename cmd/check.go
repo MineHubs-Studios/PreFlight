3d8d62a4a2b7f1771c66bad60f23cc8fea930c34
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2025-02-28 11:18:15 +0100

    Update check.go
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- cmd/check.go
+++ cmd/check.go
@@ -3,18 +3,37 @@ package cmd
 import (
 	"PreFlight/core"
 	"PreFlight/modules"
+	"context"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 var checkCmd = &cobra.Command{
 	Use:   "check",
 	Short: "Checks if all required dependencies are installed",
 	Run: func(cmd *cobra.Command, args []string) {
-		core.RegisterModule(modules.PhpModule{})
-		core.RegisterModule(modules.ComposerModule{})
-		core.RegisterModule(modules.NpmModule{})
+		err := core.RegisterModule(modules.PhpModule{})
 
-		core.RunChecks()
+		if err != nil {
+			return
+		}
+
+		err = core.RegisterModule(modules.ComposerModule{})
+
+		if err != nil {
+			return
+		}
+
+		err = core.RegisterModule(modules.NpmModule{})
+
+		if err != nil {
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+		defer cancel()
+
+		core.RunChecks(ctx)
 	},
 }
 
